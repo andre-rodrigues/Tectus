d786a61e447c8a336a1f7c0bdeff90e91c5c33b3
Andre <andremartins_r@hotmail.com>
2011-09-07 18:19:43 -0300

    Criado novo projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tectus::Application.config.secret_token = '78caa7705c8cbacc3fc3004c7d6e63fde817ee4d9c07cda053571fde68039048c8c4fa2f700c2a42f3b8f9d0175307efc956d9e8828486eb80a2d4aab55824d3'
