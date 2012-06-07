b880b118ef8207719981f5823b8821eaa2f74195
Andre <andremartins_r@hotmail.com>
2012-06-06 21:56:57 -0300

    derr

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,14 +1,11 @@
 #email = YAML.load_file("#{Rails.root}/config/credentials.yml")['email']
-email['domain']   = "randomhost.com.br"
-email['user']     = "teste@randomhost.com.br"
-email['password'] = "wtflolwut"
 
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => email['domain'],
-  :user_name            => email['user'],
-  :password             => email['password'],
+  :domain               => "randomhost.com.br",
+  :user_name            => "teste@randomhost.com.br",
+  :password             => "wtflolwut",
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
