538349f5649a3bd83ba9ce854d2e2607fa33d22e
Andre <andremartins_r@hotmail.com>
2012-06-06 21:52:41 -0300

    Removendo chamada do arquivo credentials temporariamente

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,4 +1,7 @@
-email = YAML.load_file("#{Rails.root}/config/credentials.yml")['email']
+#email = YAML.load_file("#{Rails.root}/config/credentials.yml")['email']
+email['domain']   = "randomhost.com.br"
+email['user']     = "teste@randomhost.com.br"
+email['password'] = "wtflolwut"
 
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
