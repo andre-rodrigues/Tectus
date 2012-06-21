5af8a8dbcaa8036936773fe4bd035b849ae32ff7
Andre <andremartins_r@hotmail.com>
2012-06-20 21:25:00 -0300

    removing path parameter from paper_clip options

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -2,14 +2,17 @@ class GalleryImage < Asset
   before_save :set_dimensions
 
   has_attached_file :attachment,
-                    :styles => { :admin_thumb => "94x61#", :original => "1280x1024>" },
+                    :styles => { 
+                                :admin_thumb => "94x61#", 
+                                :original => "1280x1024>" 
+                    },
                     :storage => Rails.env.production? ? :s3 : :filesystem,
                     :bucket => ENV['S3_BUCKET'],
                     :s3_credentials => {
                       :access_key_id => ENV['S3_KEY'],
                       :secret_access_key => ENV['S3_SECRET']
-                    },
-                    :path => Rails.env.production? ? "/:attachment/:id/:style/:filename" : ":rails_root/public:url"
+                    }
+                    # :path => Rails.env.production? ? "/:attachment/:id/:style/:filename" : ":rails_root/public:url"
 
   protected
     def set_dimensions
